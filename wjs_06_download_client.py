af201c60b028aaa4e788d7ebfb9fe55f020e0c1d
JasonMuscles <wangjinsenufo@qq.com>
2019-11-28 15:41:07 +0800

    tcp client and server download module

--- wjs_06_download_client.py
+++ wjs_06_download_client.py
@@ -18,12 +18,13 @@ def main():
     # 5.将文件名发送到服务器
     tcp_socket.send(download_file_name.encode("gbk"))
 
-    # 6.接受文件中的数据
-    recv_data = tcp_socket.recv(1024)  # 1024->1K（1MB = 1014*1024；1GB = 1024*1024*1024）
+    # 6.接受文件中的数据；1024->1K（1MB = 1014*1024；1GB = 1024*1024*1024）
+    recv_data = tcp_socket.recv(1024)
 
-    # 7.保存接受到的数据到一个文件中
-    with open("新_" + download_file_name, "wb") as f:
-        f.write(recv_data)
+    if recv_data:
+        # 7.保存接受到的数据到一个文件中（"wb" ==> w:write  b:二进制）
+        with open("新_" + download_file_name, "wb") as f:
+            f.write(recv_data)
 
     # 8.关闭套接字
     tcp_socket.close()
--- wjs_07_download_server.py
+++ wjs_07_download_server.py
@@ -1,6 +1,28 @@
 import socket
 
 
+def send_file_2_client(new_client_socket, client_addr):
+    # 1.接收客户端，需要下载的文件名
+    # 接受客户端发送过来需要下载的文件名称
+    file_name = new_client_socket.recv(1024).decode("gbk")
+    print("客户端:%s \n请求下载的文件名为：%s\n" % (str(client_addr), file_name))
+
+    file_content = None
+    # 2.打开这个文件,读取数据
+    try:
+        f = open(file_name, "rb")
+        file_content = f.read()
+        f.close()
+
+    except Exception as ret:
+        print("%s文件无法读取！" % file_name)
+
+    # 3.发送文件数据给客户端
+    # new_client_socket.send("服务器文件内容发送中...".encode("gbk"))
+    if file_content:
+        new_client_socket.send(file_content)
+
+
 def main():
     # 1.买个手机（创建套接字 socket）
     tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,20 +33,16 @@ def main():
 
     # 3.手机设置为正常接听状态 （让默认的套接字由主动变为被动 listen）
     tcp_server_socket.listen(128)
+    while True:
+        # 4.等待别人的电话到来（等待客户端的链接 accept）
+        new_client_socket, client_addr = tcp_server_socket.accept()
 
-    # 4.等待别人的电话到来（等待客户端的链接 accept）
-    new_client_socket, client_addr = tcp_server_socket.accept()
+        # 5.调用发送文件函数完成为客户端服务
+        send_file_2_client(new_client_socket, client_addr)
 
-    # 接受客户端发送过来需要下载的文件名称
+        # 6.关闭套接字
+        new_client_socket.close()
 
-    file_name = new_client_socket.recv(1024).decode("gbk")
-    print("客户端:%s \n请求下载的文件名为：%s\n" % (str(client_addr), file_name))
-
-    # 发送文件数据给客户端
-    new_client_socket.send("服务器文件内容发送中...".encode("gbk"))
-
-    # 关闭套接字
-    new_client_socket.close()
     tcp_server_socket.close()
 
 
